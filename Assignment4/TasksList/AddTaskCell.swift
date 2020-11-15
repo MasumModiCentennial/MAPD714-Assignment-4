de4acff63cc61c6eeb04b8023771c2292607b97c
masummodi <masum.modi@yobored.com>
2020-11-14 21:24:31 -0500

    - Added Task List Screen

---
+++ Assignment4/TasksList/AddTaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddTaskCell.swift
+//  Assignment4
+//
+//  Created by Masum Modi on 2020-11-14.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
